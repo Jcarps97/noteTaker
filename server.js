2159c36311ce03b888114723d9379731d21a82b2
Jack Carpenter <jackcarpenter1@live.com>
2021-10-21 09:31:41 -0500

    Further updating JS

--- server.js
+++ server.js
@@ -5,33 +5,38 @@ const PORT = process.env.PORT || 3001;
 const { readAndAppend } = require("./utils/fsUtils");
 let allNotes = require("./db/db.json");
 
+//Baseline code that should be used in all call files
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-
+//Getting note file
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/notes.html"));
 });
 
-
+//Pulling Note history
 app.get('/api/notes', (req, res) => {
     res.json(allNotes);
 });
 
+//Creating new notes
 app.post('/api/notes', (req, res) =>{
     if (req.body) {
         readAndAppend(req.body, './db/db.json');
         res.json('Note added successfully');
+        //Side column does not update
     } else {
-        res.error('Error, cannot add note');
+        res.json('Error, cannot add note');
     }
 });
 
+//Wildcard call
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "./public/index.html"));
 })
 
+//Port listener
 app.listen(PORT, () => {
     console.log(`listening at http://localhost:${PORT}`);
 })
